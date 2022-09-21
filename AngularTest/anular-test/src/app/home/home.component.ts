cabd3ac9763f4fdf5fe476c93385e4b470e9aca4
Anton Ageev <tria2006@mail.ru>
2022-09-21 21:53:55 +0300

    add angular

---
+++ AngularTest/anular-test/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.http.get<any>('https://localhost:44374/Test/GetNumber').subscribe(res => {
+      var a = 0;
+    })
+  }
+}
