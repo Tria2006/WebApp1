7207238029bc28f3aa6f1feac4cf1cb7b4961d1c
Anton Ageev <tria2006@mail.ru>
2022-09-22 12:27:14 +0300

    fix CORS error for front -> back requests

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -26,7 +26,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -47,6 +47,10 @@ namespace WebApplication1
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(
+                options => options.WithOrigins("*").AllowAnyMethod()
+            );
 
             app.UseAuthorization();
 
